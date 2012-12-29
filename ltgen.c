e6ea8b1b25359f5df6dd3915ed6c5915dc25586d
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-12-29 11:33:36 +0100

    Added --version and --help

--- ltgen.c
+++ ltgen.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 static void usage(int argc, char** argv)
 {
@@ -18,6 +19,26 @@ static void usage(int argc, char** argv)
 
 int main(int argc, char** argv)
 {
-	usage(argc, argv);
+	if (argc == 1 || !strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+	{
+		usage(argc, argv);
+		exit(0);
+	}
+	else if (!strcmp(argv[1], "--version") || !strcmp(argv[1], "-v"))
+	{
+		printf("ltgen\n");
+		printf("Compiled on %s at %s\n", __DATE__, __TIME__);
+		exit(0);
+	}
+
+	if (argc < 3)
+	{
+		usage(argc, argv);
+		exit(0);
+	}
+
+	char* charset = "0123456789abcdefghijklmnopqrstuvwxyz";
+	(void) charset;
+
 	return 0;
 }
